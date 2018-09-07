d3bc163ee3960424d0f546507508273fee71a584
MicahWeiss <micahjweiss@gmail.com>
2018-09-07 02:41:55 -0500

    Stringify complete. GEBCN has a syntax error.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var recursiveGEBCN = function(node, className){
+    console.log(node);
+    return [];
+    //Base case: No children
+    if(node.childNodes.length === 0){
+      if(node.classList.contains(className)){
+        return [node]; //returing an array containing node
+      }
+      return []; 
+    }
+    
+    //Recursive case: has children
+    var outputNodeArray = [];
+    for(var i=0; i< node.childNodes.length; i++){
+      outputNodeArray.concat(recursiveGEBCN(node.childNodes[i], className));
+    }
+    return outputNodeArray;  
+  }
+  //console.log(recursiveGEBCN(document.body, className));
+  return recursiveGEBCN(document.body, className);
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  //primitive
+  if(['boolean', 'number'].includes(typeof obj)){
+    return String(obj);
+  }
+  if(typeof obj === 'string'){ //case separate for formatting
+    return '\"' + obj + '\"';
+  }
+  //Null
+  if([NaN, null, Infinity].includes(obj)){
+    return 'null';
+  }
+  //Array (recursive for nesting)
+  if(Array.isArray(obj)){
+    var output = '[';
+    for(var i=0; i< obj.length; i++){
+      output += stringifyJSON(obj[i]);
+      if(i < obj.length -1){
+       output += ',';
+      }
+    }
+    output += ']';
+    return output;
+  }
+  
+  //Object (recursive for nesting)
+  var output = '{';
+  var keys = Object.keys(obj);
+
+  for(var i = 0; i < keys.length; i++){
+    if(keys[i] !== "functions" && keys[i] !== "undefined"){
+      output += '\"' + keys[i] + '\":';
+      output += stringifyJSON(obj[keys[i]]);      
+      if(i < keys.length -1){
+        output += ',';
+      }
+    }
+  }
+  output += '}';
+  return output; 
+}
