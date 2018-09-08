4863b72554f58c5bbed9c1a87b3a81b3e6002084
MicahWeiss <micahjweiss@gmail.com>
2018-09-08 05:26:34 -0500

    Outputs an answer, but still not correct

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,24 +5,36 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
+  //console.log(recursiveGEBCN(document.body, className));    
   var recursiveGEBCN = function(node, className){
-    console.log(node);
-    return [];
     //Base case: No children
+    //console.log('classList: ',node.classList);
     if(node.childNodes.length === 0){
+      if(node.classList === undefined){
+        //console.log('no classes');
+        return [];
+      }
       if(node.classList.contains(className)){
+        //console.log('found a match');
         return [node]; //returing an array containing node
       }
-      return []; 
+      else{
+        //console.log('no match found');
+        return [];   
+      }
     }
     
     //Recursive case: has children
     var outputNodeArray = [];
-    for(var i=0; i< node.childNodes.length; i++){
+    if(node.classList.contains(className)){//check current node
+      outputNodeArray.push(node);
+    }
+    for(var i=0; i< node.childNodes.length; i++){//iterate thru children
       outputNodeArray.concat(recursiveGEBCN(node.childNodes[i], className));
     }
     return outputNodeArray;  
   }
-  //console.log(recursiveGEBCN(document.body, className));
+  console.log(recursiveGEBCN(document.body, className));
+  console.log(document.getElementsByClassName(className));
   return recursiveGEBCN(document.body, className);
 };
