f43e264d3e30aa4bd7108e427e3f6d576d9ba7f6
MicahWeiss <micahjweiss@gmail.com>
2018-09-09 19:41:49 -0500

    Mission complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,35 +6,26 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   //console.log(recursiveGEBCN(document.body, className));    
-  var recursiveGEBCN = function(node, className){
-    //Base case: No children
+  var outputNodeArray = [];
+  var recursiveGEBCN = function(node){
     //console.log('classList: ',node.classList);
-    if(node.childNodes.length === 0){
-      if(node.classList === undefined){
-        //console.log('no classes');
-        return [];
-      }
+    if (node.classList !== undefined){
       if(node.classList.contains(className)){
-        //console.log('found a match');
-        return [node]; //returing an array containing node
+        //console.log('Match found: ', node.tagName);
+        outputNodeArray.push(node);
       }
-      else{
-        //console.log('no match found');
-        return [];   
-      }
-    }
-    
-    //Recursive case: has children
-    var outputNodeArray = [];
-    if(node.classList.contains(className)){//check current node
-      outputNodeArray.push(node);
-    }
+    }  
+    /*if(node.childNodes.length > 0){
+      console.log('checking '+ node.childNodes.length + ' children of ', node.tagName);
+    }*/ 
     for(var i=0; i< node.childNodes.length; i++){//iterate thru children
-      outputNodeArray.concat(recursiveGEBCN(node.childNodes[i], className));
+      recursiveGEBCN(node.childNodes[i]);
     }
-    return outputNodeArray;  
   }
-  console.log(recursiveGEBCN(document.body, className));
-  console.log(document.getElementsByClassName(className));
-  return recursiveGEBCN(document.body, className);
+  
+  //console.log('before ', outputNodeArray);
+  recursiveGEBCN(document.body);
+  //console.log('after ', outputNodeArray);
+  //console.log(Array.prototype.slice.apply(document.getElementsByClassName(className)));
+  return outputNodeArray;
 };
